b75b0aa817e640e5100f191e3988f518f8ddd373
manasesortez <manasesortez1234@hotmail.com>
2021-10-16 10:09:05 -0600

    Fragments

---
+++ app/src/main/java/com/amtodev/exserciseactionbar/Configuracion.java
@@ -0,0 +1,69 @@
+package com.amtodev.exserciseactionbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class Configuracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_group_24);
+        toolbar.setTitle("Configuracion");
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"your icon was clicked",Toast.LENGTH_SHORT).show();
+                Intent back = new Intent(Configuracion.this, MainActivity.class);
+                startActivity(back);
+                finish(); 
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        switch(item.getItemId()){
+            case R.id.item1:
+                Intent add = new Intent(this, Registro.class);
+                startActivity(add);
+                finish();
+                break;
+            case R.id.item2:
+                Intent config = new Intent(this, Configuracion.class);
+                startActivity(config);
+                finish();                break;
+            case R.id.item3:
+                Intent about = new Intent(this, acercaDe.class);
+                startActivity(about);
+                finish();                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
